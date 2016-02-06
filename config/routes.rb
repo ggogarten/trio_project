ad08fb3dc54047b2ed7860a963948456900f882d
Matt King <mattdanking@gmail.com>
2016-02-06 18:30:24 -0500

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'alchemy#index'
   post 'analyze' => 'alchemy#connect'
   # The priority is based upon order of creation: first created -> highest priority.
